7123ffbab8d238eb337ff3d5bc3ff303d3cc3ae3
Ali Chaaban <alivideogamer@hotmail.com>
2017-05-12 14:10:28 +0300

    modify controller

--- foodtruckaliService/DataObjects/TodoItem.cs
+++ foodtruckaliService/DataObjects/TodoItem.cs
@@ -4,8 +4,9 @@ namespace foodtruckaliService.DataObjects
 {
     public class TodoItem : EntityData
     {
+        
         public string Text { get; set; }
-
+        public bool IsAvailable { get; set; }
         public bool Complete { get; set; }
     }
 }
